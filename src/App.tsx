16ae0d22f85c53fd4e8b4980bd3dcf1b4c573845
jerncomania28 <okonnduesojeremiah@gmail.com>
2023-08-03 19:50:52 +0100

    feat: initialization of elud project

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const App: React.FC = () => {
+  return (
+    <div>
+      <h1 className="text-4xl text-green-400"> Elud Financial App</h1>
+    </div>
+  );
+};
+
+export default App;
